30e271ef9c22bb04a47a4e9ae041e3ef1e8848e0
Gabor Salamon <32394903+Kormirim@users.noreply.github.com>
2018-01-05 16:25:40 +0100

    Added Mocha test script for removeSchedule().

---
+++ test/index-remove-mocha.js
@@ -0,0 +1,78 @@
+
+/*
+ * Mocha script to test schedlue managing functions.
+ * Run it like that: mocha test/index-remove-mocha.js
+ */
+
+var assert = require('assert'),
+	 noCron = require("../src");
+
+config = {
+	// Defining job templates we will schedule single/multiple times, during the test process.
+	jobs: [{
+		"name": "mock-task-01",
+		"schedule": {
+			"duration": 10,
+			"unit": "s"
+		},
+		"module": "./mocks/task",
+		"method": "run",
+		"params": []
+	 }, {
+		"name": "mock-task-02",
+		"schedule": {
+			"duration": 10,
+			"unit": "s"
+		},
+			"module": "./mocks/task",
+			"method": "run",
+			"params": []
+		}],
+	"secondsToWaitBeforeTestRemoving": 60
+};
+
+describe('Managing schedules', function () {
+
+	describe('addSchedule', function () {
+
+		config.jobs.forEach(function (job, i) {
+			var params = [null].concat(job.params),
+				mod = require(job.module),
+				method = mod[job.method].bind.apply(mod[job.method], params);
+
+			noCron.addSchedule(job, method);
+		});
+
+
+		var jobs = noCron.getSchedules();
+
+		it('should the number of jobs we have just scheduled be two.', function () {
+			assert.equal(jobs.length, 2);
+		});
+		it('should the name of job1 be mock-task-01.', function () {
+			assert.equal(jobs[0]._name, 'mock-task-01');
+		});
+		it('should the name of job2 be mock-task-02.', function () {
+			assert.equal(jobs[1]._name, 'mock-task-02');
+		});
+
+ 	});
+
+	describe('removeSchedule', function () {
+
+		var jobsBefore = noCron.getSchedules();
+		noCron.removeSchedule('mock-task-01');
+		var jobsAfter = noCron.getSchedules();
+
+		it('should decrease the number of jobs', function () {
+			if (jobsAfter.length >= jobsBefore.length) {
+				assert.fail(2, undefined, 'Number of jobs after removing mock-task-01 is not less then before. Before: ' + jobsBefore.length + ', after: ' + jobsAfter.length + '.');
+			}
+		});
+
+		it('should remove mock-task-01 and keep mock-task-02', function () {
+			assert.equal(jobsAfter[0]._name, 'mock-task-02');
+		});
+
+ 	});
+});
